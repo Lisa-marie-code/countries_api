cd86502cb8c0237749beedf15569f6cad9f33b60
Lisa-marie-code <lisamariekoomson340@gmail.com>
2023-02-20 10:07:27 +0000

    first commit

---
+++ rest_countries_api/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ".//App.css"
+import {Theme} from "./Components/Header/header"
+
+export const App = () =>{
+  return(
+    <div>
+      <Theme/>
+    </div>
+  )
+}
+
+export default App;
---
+++ rest_countries_api/src/Components/Header/header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export const Theme = () =>{
+    return(
+        <div>
+            <div>
+                <h2>Where in the world?</h2>
+                <div>
+                <i class="fa fa-moon-o" aria-hidden="true"></i>
+                <p>Dark Mode</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Theme;
