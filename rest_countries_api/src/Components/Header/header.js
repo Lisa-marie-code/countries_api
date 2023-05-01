83056941e1ea0de5d4a8711fe9932982c76518e7
Lisa-marie-code <lisamariekoomson340@gmail.com>
2023-05-01 14:35:25 +0000

    updated theme

--- rest_countries_api/src/Components/Header/header.js
+++ rest_countries_api/src/Components/Header/header.js
@@ -1,40 +1,27 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import { FaMoon } from "react-icons/fa";
 import "./header.css";
 
 export const Theme = () => {
-  const [theme, setTheme] = useState("light");
-
-  useEffect(() => {
-    const body = document.querySelector("body");
-    const header = document.querySelector(".header");
-    const sections = document.querySelectorAll(".each-country");
-    body.classList.toggle("dark-theme", theme === "dark");
-    header.classList.toggle("dark-theme", theme === "dark");
-
-    sections.forEach((section) => {
-      section.classList.toggle("dark-theme", theme === "dark");
-    });
-  }, [theme]);
-
-  useEffect(() => {
-    const savedTheme = localStorage.getItem("theme");
-    if (savedTheme) {
-      setTheme(savedTheme);
-    }
-  }, []);
-
+  const [theme, setTheme] = useState(
+    localStorage.getItem("theme") || "light");
   const toggleTheme = () => {
-    const newTheme = theme === "light" ? "dark" : "light";
-    localStorage.setItem("theme", newTheme);
-    setTheme(newTheme);
+    if (theme === "light") {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
   };
-
+  useEffect(() => {
+    localStorage.setItem("theme", theme);
+    document.body.className = theme;
+  }, [theme]);
+  
   return (
     <div>
       <header className="header">
         <h2>Where in the world?</h2>
-        <div className="moon">
+        <div className="moon" >
           <FaMoon className="moon-icon" />
           <h3 onClick={toggleTheme}>Dark Mode</h3>
         </div>
