304414dff0141bbb0329eb8da5914393cb26a16f
Lisa-marie-code <lisamariekoomson340@gmail.com>
2023-05-03 08:53:00 +0000

    colors rightly added

--- rest_countries_api/src/Components/Borders/border.js
+++ rest_countries_api/src/Components/Borders/border.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
+import "./border.css"
 
 export const BorderCountries = () => {
   const { state } = useLocation();
